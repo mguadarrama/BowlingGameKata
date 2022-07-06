af9e0158c72b2446e07de57776e3bb14129b08a9
Manu <manuel.jonathan@gmail.com>
2022-07-06 04:51:12 +0200

    first commit

---
+++ BowlingGameKata/BowlingGameTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace BowlingGameKata
+{
+    public class BowlingGameTest
+    {
+        private Game _testee;
+
+        public BowlingGameTest()
+        {
+            _testee = new Game();
+        }
+        [Fact]
+        public void Roll_ShouldReturnNumberOfRolledPins()
+        {
+            _testee.Roll(5);
+            _testee.Score.Should().Be(5);
+        }
+    }
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace BowlingGameKata
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+
+        public int Score { get; private set; }
+
+
+    }
+}
